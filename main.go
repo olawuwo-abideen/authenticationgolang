bfaf5cbbe58b07988dd784e7202251a8ac58ecb8
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-06-13 15:20:30 +0100

    update

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	routes "github.com/olawuwo-abideen/golangauthentication/routes"
 )
 
 func main() {
@@ -22,5 +23,18 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
 
 }
